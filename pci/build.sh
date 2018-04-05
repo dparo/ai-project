c265913cefa29615be266d469e8b7936a3f93ec7
Davide Paro <davide.paro@studenti.unipd.it>
2018-04-05 13:29:09 +0200

    Removed useless files deriving from other projects.

--- pci/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-gcc -D_GNU_SOURCE -D__DEBUG -std=gnu11 -O0 -g3 -Wall -Wextra -Wno-unused-function -Wno-unused-parameter -Wno-unused-variable -DMAIN_C_IMPL -I./ -I./__generated__ -I../common main.c -lm -ldl -pthread -o ./bin/http_chunked
