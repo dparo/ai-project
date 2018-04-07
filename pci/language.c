e3c982841cef5d07952a174a4ff2e0468d13985a
Davide Paro <paro.dav.95@gmail.com>
2018-04-07 13:29:15 +0200

    Right associative implication `->` operator. 1 more interesting formula to test.

--- pci/language.c
+++ pci/language.c
@@ -51,7 +51,9 @@ static const struct operator_infos {
     [TT_PUNCT_BITWISE_NOT]   = { 2, 1, LEFT_ASSOCIATIVE_OP, PREFIX_OP },
 
     [TT_PUNCT_BOTHDIR_ARROW] = { 1, 2, LEFT_ASSOCIATIVE_OP, INFIX_OP },
-    [TT_PUNCT_ARROW]         = { 1, 2, LEFT_ASSOCIATIVE_OP, INFIX_OP },
+    // C-SPEC defines the arrow to be left associative. In propositional
+    // calculus we use implication `->` right associative
+    [TT_PUNCT_ARROW]         = { 1, 2, RIGHT_ASSOCIATIVE_OP, INFIX_OP },
 
 
     
