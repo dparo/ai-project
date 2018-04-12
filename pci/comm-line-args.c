25d0a53e5c19fe1d201e4e7dc5367869bf0581dc
Davide Paro <davide.paro@studenti.unipd.it>
2018-04-12 13:49:59 +0200

    Added dblog for comm line args parsing.

--- pci/comm-line-args.c
+++ pci/comm-line-args.c
@@ -16,36 +16,50 @@ const char *argp_program_version = "0.1";
 error_t argp_error_exit_status = EX_USAGE;
 
 
-#define SOLVER_ARGP_KEY 's'
-
 static const struct argp_option opts[] = {
     // name, key, arg, flags, doc, group
-    { "solver", SOLVER_ARGP_KEY, "SOLVER", OPTION_ARG_OPTIONAL | 0, "Specify the solver to use: {bruteforce | SLD}", 1},
+    { "solver", 's', "SOLVER", OPTION_ARG_OPTIONAL | 0, "Specify the solver to use: {bruteforce | SLD}", 1},
     {0}
 };
 
-error_t parser (int key, char *arg, struct argp_state *state)
+static inline void
+commline_dbglog(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+error_t
+parser (int key, char *arg, struct argp_state *state)
 {
     if ( key == ARGP_KEY_INIT ) {
         // This is always the first time this function will get called
         // can do the initialization here
+        commline_dbglog("ARGP_KEY_INIT\n");
     } if ( key == ARGP_KEY_NO_ARGS ) {
         // No argument passed to the program
+        commline_dbglog("ARGP_KEY_NO_ARGS\n");
     } else if ( key == ARGP_KEY_END ) {
         // End of parsing
+        commline_dbglog("ARGP_KEY_END\n");
     } else if ( key == ARGP_KEY_SUCCESS ) {
         // Command line reading was successfull
+        commline_dbglog("ARGP_KEY_SUCCESS\n");
     } else if ( key == ARGP_KEY_FINI ) {
         // Final time this function will ever get called
         // can do free's here
+        commline_dbglog("ARGP_KEY_FINI\n");
     } else {
         switch (key) {
 
-        case SOLVER_ARGP_KEY: {
+        case 's': {
             printf("Setting the solver to: %s\n", arg);
         } break;
             
         default: {
+            commline_dbglog("ARGP_ERR_UNKNOWN\n");
             return ARGP_ERR_UNKNOWN;
         } break;
         }
