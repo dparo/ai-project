93b850dda62cced9647400fd2e1ea350a1180c56
Davide Paro <davide.paro@studenti.unipd.it>
2018-04-18 23:10:00 +0200

    More implementations.

--- pci/code-gen/stack.template.c
+++ pci/code-gen/stack.template.c
@@ -8,6 +8,7 @@ struct $(S) {
 struct $(S)
 $(S)_init_sized(size_t num_elems)
 {
+    assert(num_elems);
     struct $(S) result;
     size_t buf_size = num_elems * sizeof($(T));
     result->base = xmalloc(buf_size);
@@ -27,6 +28,7 @@ static inline void
 $(S)_grow_to( struct $(S) *s,
               size_t new_max_elems) /* Size in BYTES !!! */
 {
+    assert(s);
     size_t new_size = new_max_elems * sizeof($(T)) * 2;
     void *temp = xrealloc(s->base, newsize);
     assert(temp);
@@ -37,6 +39,7 @@ $(S)_grow_to( struct $(S) *s,
 static inline void
 $(S)_grow(struct $(S) *s)
 {
+    assert(s);
     const size_t new_max_elems = s->max_elems * 2;
     $(S)_grow_to(s, new_size);
 }
@@ -44,12 +47,14 @@ $(S)_grow(struct $(S) *s)
 static inline size_t
 $(S)_num_elems(struct $(S) *s)
 {
+    assert(s);
     return s->num_elems;
 }
 
 static inline bool
 $(S)_is_empty(struct $(S) *s)
 {
+    assert(s);
     return $(S)_num_elems(s);
 }
 
@@ -57,6 +62,7 @@ $(S)_is_empty(struct $(S) *s)
 $(T) *
 $(S)_peek_addr (struct $(S) *s)
 {
+    assert(s);
     assert(s->num_elems >= 0);
     return & (s->base[(s->num_elems)]);
 }
@@ -66,6 +72,7 @@ $(S)_peek_addr (struct $(S) *s)
 $(T) *
 $(S)_pop_addr (struct $(S) *s)
 {
+    assert(s);
     $(T) *result = $(S)_peek_addr(s);
     (s->num_elems)--;
     return result;
@@ -74,6 +81,7 @@ $(S)_pop_addr (struct $(S) *s)
 $(T)
 $(S)_pop (struct $(S) *s)
 {
+    assert(s);
     assert(s->num_elems >= 0);
     return s->base[--(s->num_elems)];
 }
@@ -82,6 +90,7 @@ static inline bool
 $(S)_enough_size_to_hold_n(struct $(S) *s,
                            size_t n)
 {
+    assert(s);
     return (& s->base[s->num_elems + n]) == & (s->base[s->max_elems]);
 }
                  
@@ -90,6 +99,8 @@ void
 $(S)_push( struct $(S) *s,
            $(T) *elem )
 {
+    assert(elem);
+    assert(s)
     if ( ! $(S)_enough_size_to_hold_n(s, 1)) {
         $(S)_grow(s);
     }
@@ -100,18 +111,41 @@ $(S)_push( struct $(S) *s,
 static inline $(T) *
 $(S)_begin(struct $(S) *s)
 {
+    assert(s);
     return s->base;
-}
-
-static inline $(T) *
-$(S)_next($(T) *prev)
-{
-    return ($(T)) ((uint8_t*) prev + sizeof(prev));
 }
 
 
 $(T) *
 $(S)_end(struct $(S) *s)
 {
+    assert(s);
     return & (s->base[s->num_elems]);
+}
+
+
+
+
+static inline $(T) *
+$(S)_next($(T) *prev)
+{
+    assert(prev);
+    return ($(T)) ((uint8_t*) prev - sizeof(prev));
+}
+
+
+
+static inline $(T) *
+$(S)_prev($(T) *prev)
+{
+    assert(prev);
+    return ($(T)) ((uint8_t*) prev + sizeof(prev));
+}
+
+
+for ( ast_node *node = ast_end(ast);
+      node != ast_begin();
+      node = ast_prev(node))
+{
+     
 }
