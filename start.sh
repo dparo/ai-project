398c07bcf8569966d76f226c6a9bbe59f2ecb67b
Davide Paro <paro.dav.95@gmail.com>
2018-04-22 16:07:03 +0200

    Changed the start file to set the compile command.

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # Fai partire i tool del mestiere.
 # Prepararsi a una lunga nottata.
 
-emacs --execute "(compile \"cd $PWD/src/ && ./build.sh debug\")" "./src/sat/main.c" &
+
+emacs --execute "(set 'compile-command \"cd $PWD/src/ && ./build.sh debug\")" "./src/sat/main.c" &
 urxvt -e ddd -f ./bin/pci &
 exit
